f97c4a7b53703d905b8ff14c9c1e258d380cdabb
hzy-star <3312943310@qq.com>
2024-12-30 17:06:37 +0800

    修改paas文件

--- gears/paas-bootstrap.sh
+++ gears/paas-bootstrap.sh
@@ -2,9 +2,10 @@
 export PATH=/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin
 set -xv
 
+mkdir -p /dianyi/app/newbee-web/
 tar -zxvf /root/newbee-web.tar.gz /dianyi/app/newbee-web/
 
 # init log file
 /bin/bash /root/ypp_log_init.sh
 
-/usr/sbin/nginx  -s reload
+# /usr/sbin/nginx  -s reload
