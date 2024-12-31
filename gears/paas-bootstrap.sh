e65026fd3407431643bbedf7d49e724588176455
hzy-star <3312943310@qq.com>
2024-12-31 11:20:50 +0800

    修改nginx

--- gears/paas-bootstrap.sh
+++ gears/paas-bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 export PATH=/sbin:/usr/sbin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin
 set -xv
-
+# 在文件开头添加环境变量检查
+echo "Current BACKEND_API_URL: ${BACKEND_API_URL}"
 # 使用 envsubst 处理配置文件
 envsubst '${BACKEND_API_URL}' < /etc/nginx/conf.d/manage.ym.conf > /etc/nginx/conf.d/manage.ym.conf.tmp
+cat /etc/nginx/conf.d/manage.ym.conf.tmp  # 调试用，查看替换结果
 mv /etc/nginx/conf.d/manage.ym.conf.tmp /etc/nginx/conf.d/manage.ym.conf
-
 # mkdir -p /dianyi/app/newbee-web/
 # tar -zxvf /root/newbee-web.tar.gz /dianyi/app/newbee-web/
 
